1474f47e909e3bd89c3cd33f4fc327f1e2e03fff
Jigumina <125941168+Jigumina@users.noreply.github.com>
2023-03-06 14:33:26 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,16 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner teclado = new Scanner(System.in);
-        System.out.println("Introduce 9 caracteres en mayúsculas para el juego:");
+        System.out.println("Introduce 9 caracteres en mayúsculas para el juego (estas pueden ser X,O,_):");
         String caracteres = teclado.nextLine();
 
         int cadena = caracteres.length();
+        char X = 'X';
+        char O = 'O';
+        char blanco = '_';
 
-        if (cadena == 9) {
+        if (cadena == 9 && (caracteres.charAt(0) == X) || (caracteres.charAt(0) == X) || (caracteres.charAt(0) == blanco))
+        {
 
             System.out.println(" ----- ");
             System.out.println("| " + caracteres.charAt(0) + caracteres.charAt(1) + caracteres.charAt(2) + " |");
@@ -18,7 +22,7 @@ public class Main {
             System.out.println(" ----- ");
 
         } else {
-            System.out.println("Se necesitan 9 caracteres");
+            System.out.println("Se necesitan 9 caracteres y que sean X,O,_");
         }
     }
 }
